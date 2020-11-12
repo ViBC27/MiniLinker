49ed2d27728b165839433fb05d3d6e0644cae0b6
Vitor Barcelos <vbc@ic.ufal.br>
2020-11-12 14:34:13 -0300

    feat(application): refactor the application scaffolding

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
+import morgan from 'morgan';
 import express from 'express';
-
-import './database/connection';
 import routes from './routes';
+import './database/connection';
 
 const app = express();
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000);
+app.listen(3000, () => console.log('Active in 3000'));
